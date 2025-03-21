a3eadaff57412d535a83cc89716b248c19f0b5bf
ruby <2bluesky2@naver.com>
2025-03-21 11:32:09 +0900

    feat: 필요한 클래스 추가

--- bin/dart_rpg_game.dart
+++ bin/dart_rpg_game.dart
@@ -1,5 +1,26 @@
 import 'package:dart_rpg_game/dart_rpg_game.dart' as dart_rpg_game;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_rpg_game.calculate()}!');
+void main() {
+  // 1. 파일로부터 데이터 읽어오기
+  // 1-1. characters.txt. 에서 체력, 공격, 방어력 읽어오기
+  // 1-2 monsters.txt 에서 이름, 체력, 공격력 최대값 읽어오기
+  //2. 사용자로부터 캐릭터 이름 입력받기 기능
+  //특수문자나 숫자가 포함되지 않아야함
+  //3. 게임 종료 후 결과를 파일에 저장
+  
 }
+
+
+/// 게임을 정의하기 위한 클래스 : Game
+class Game{
+  //자동정렬
+}
+/// 캐릭터를 정의하기 위한 클래스 : Character
+class Character{
+  //
+}
+/// 몬스터를 정의하기 위한 클래스 : Monster
+class Monster{
+  //
+}
+
